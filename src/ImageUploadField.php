786c60e566b6a37d3caf1b8fb1665fb87b47c265
Nic Horstmeier <nhorstmeier@dynamicagency.com>
2019-07-22 20:19:52 -0500

    UPDATE SilverStripe 4 compatibility

---
+++ src/ImageUploadField.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Dynamic\ImageUpload;
+
+use SilverStripe\AssetAdmin\Forms\UploadField;
+use SilverStripe\Core\Convert;
+use SilverStripe\Core\Injector\Injector;
+use SilverStripe\ORM\SS_List;
+
+/**
+ * Class ImageUploadField
+ * @package Dynamic\SilverStripe\ImageUpload
+ */
+class ImageUploadField extends UploadField
+{
+    /**
+     * @var int
+     */
+    private static $max_upload = 1024000;
+
+    /**
+     * @var string
+     */
+    private static $category = 'image/supported';
+
+    /**
+     * @var array
+     */
+    private static $allowed_categories = [
+        'image/supported',
+        'image',
+    ];
+
+    /**
+     * ImageUploadField constructor.
+     * @param string $name
+     * @param null $title
+     * @param SS_List|null $items
+     */
+    public function __construct($name, $title = null, SS_List $items = null)
+    {
+        parent::__construct($name, $title);
+
+        // set the max upload size
+        $maxUpload = $this->config()->get('max_upload');
+        $maxPost = Convert::memstring2bytes(ini_get('post_max_size'));
+        $this->getValidator()->setAllowedMaxFileSize(min($maxUpload, $maxPost));
+
+        // other image specific options
+        $this->setAllowedMaxFileNumber(1);
+
+        $category = Injector::inst()->get(static::class)->config()->get('category');
+
+        if (!in_array($this->config()->get('allowed_categories'), $category)) {
+            user_error("{$category} not listed in ImageUploadField::allowed_categories. Defaulting to \"image/supported\"");
+            $category = 'image/supported';
+        }
+
+        $this->setAllowedFileCategories($category);
+    }
+}
